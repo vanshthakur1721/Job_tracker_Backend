9c955469e609059f5eec70b803c05971e27ea925
vansh <thakurvansh872@gmail.com>
2025-09-23 11:47:08 +0530

    l

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 // }));
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://job-tracker-helper.vercel.app"
+  "https://job-tracker-zeta-one.vercel.app"
 ];
 
 app.use(
