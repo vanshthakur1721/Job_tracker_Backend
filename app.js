5481eb694fd511899506eb3683f095583df424d5
vansh <thakurvansh872@gmail.com>
2025-09-23 11:07:09 +0530

    last

--- app.js
+++ app.js
@@ -14,13 +14,32 @@ const PORT = process.env.PORT || 3000;
  app.get('/', (req, res) => {
   res.send('Hello World these side vansh!')
 })
- app.use(cors({
-  origin: ["http://localhost:5173",
-   "https://job-tracker-helper.vercel.app"], // frontend URL
-  credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]
+//  app.use(cors({
+//   origin: ["http://localhost:5173",
+//    "https://job-tracker-helper.vercel.app"], // frontend URL
+//   credentials: true,
+//     allowedHeaders: ["Content-Type", "Authorization"]
 
-}));
+// }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://job-tracker-helper.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.use('/api/v1/job',jobRoutes)
 
